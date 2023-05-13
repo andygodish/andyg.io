3325b9dfc91451b2b5532a3acdec9fc553c8b620
Andy Godish <agodish18@gmail.com>
2023-05-13 14:35:25 -0600

    fix: disabled the github plugin to try to prevent release creation

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ const config = {
       'assets': ['package.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
-    '@semantic-release/github'
+    // '@semantic-release/github'
   ]
 };
 

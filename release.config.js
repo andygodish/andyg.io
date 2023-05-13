75e42cfc01f96d62782a4e107a6ff5dce6674f31
Andy Godish <agodish18@gmail.com>
2023-05-13 14:50:44 -0600

    fix: test fix config

--- release.config.js
+++ release.config.js
@@ -5,7 +5,7 @@ const config = {
     '@semantic-release/release-notes-generator',
     ['@semantic-release/npm', {
         'npmPlublish': false
-    }]
+    }],
     ['@semantic-release/git', {
       'assets': ['package.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'

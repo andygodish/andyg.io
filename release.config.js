752b9e361339188fc79f22dec0c420e1157a440f
Andy Godish <agodish18@gmail.com>
2023-05-14 18:57:36 -0600

    fixing release.yml env variable

--- release.config.js
+++ release.config.js
@@ -11,7 +11,7 @@ const config = {
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ['@semantic-release/exec', {
-      'prepareCmd': 'echo "${nextRelease.version}" > VERSION'
+      'prepareCmd': 'echo "VERSION=${nextRelease.version}" >> $GITHUB_ENV'
     }]
   ]
 };
